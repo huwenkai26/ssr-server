b63ef43f6d8536039a7d12b0c5a035979b2eb6f8
hwk <huwenkai@yuntutv.net>
2019-01-07 19:21:22 +0800

    run-ssr.sh

--- run-ssr.sh
+++ run-ssr.sh
@@ -1,59 +1,61 @@
 #! /bin/bash
-#check_os() {
-#    if [[ -f /etc/redhat-release ]]; then
-#        os="centos"
-#    elif cat /etc/issue | grep -Eqi "debian"; then
-#        os="debian"
-#    elif cat /etc/issue | grep -Eqi "ubuntu"; then
-#        os="ubuntu"
-#    elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-#        os="centos"
-#    elif cat /proc/version | grep -Eqi "debian"; then
-#        os="debian"
-#    elif cat /proc/version | grep -Eqi "ubuntu"; then
-#        os="ubuntu"
-#    elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
-#        os="centos"
-#    fi
-#}
-#
-#while true
-#    do
-#    echo "Please enter password for host:"
-#    read -p "请输入服务器ip :" host
-#    if  [ ! -n "$host" ] ;then
-#        echo -e "[${red}Error${plain}] 请输入ip"
-#        continue
-#    fi
-#
-#    echo
-#    echo "---------------------------"
-#    echo  host = ${host}
-#    echo "---------------------------"
-#    echo
-#    break
-#done
-#while true
-#    do
-#    echo "ssh 端口:"
-#    read -p "ssh端口号 :" port
-#    if  [ ! -n "$port" ] ;then
-#        echo -e "[${red}Error${plain}] 请输入端口号"
-#        continue
-#    fi
-#
-#    echo
-#    echo "---------------------------"
-#    echo  port = ${port}
-#    echo "---------------------------"
-#    echo
-#    break
-#done
-##将私钥上传至服务器
-#ssh-copy-id -i ~/.ssh/id_rsa.pub root@${host} -p ${port}
+check_os() {
+    if [[ -f /etc/redhat-release ]]; then
+        os="centos"
+    elif cat /etc/issue | grep -Eqi "debian"; then
+        os="debian"
+    elif cat /etc/issue | grep -Eqi "ubuntu"; then
+        os="ubuntu"
+    elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+        os="centos"
+    elif cat /proc/version | grep -Eqi "debian"; then
+        os="debian"
+    elif cat /proc/version | grep -Eqi "ubuntu"; then
+        os="ubuntu"
+    elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+        os="centos"
+    fi
+}
+
+while true
+    do
+    echo "Please enter password for host:"
+    read -p "请输入服务器ip :" host
+    if  [ ! -n "$host" ] ;then
+        echo -e "[${red}Error${plain}] 请输入ip"
+        continue
+    fi
+
+    echo
+    echo "---------------------------"
+    echo  host = ${host}
+    echo "---------------------------"
+    echo
+    break
+donegit
+while true
+    do
+    echo "ssh 端口:"
+    read -p "ssh端口号 :" port
+    if  [ ! -n "$port" ] ;then
+        echo -e "[${red}Error${plain}] 请输入端口号"
+        continue
+    fi
+
+    echo
+    echo "---------------------------"
+    echo  port = ${port}
+    echo "---------------------------"
+    echo
+    break
+done
+#将私钥上传至服务器
+ssh-copy-id -i ~/.ssh/id_rsa.pub root@${host} -p ${port}
 
 git clone https://github.com/huwenkai26/ssr-server.git
-
-scp -P 20292  -r ssr-server root@157.52.202.18:~/
-#rm -rf ssr-server
-#ssh root@${host}  -p ${port}  "cd ~/;ssr-server/ss-fly.sh -ssr"
+cd ssr-server
+rm -rf .git
+cd ..
+scp -P  ${port}  -r ssr-server root@${host}:~/
+rm -rf ssr-server
+ssh root@${host}  -p ${port}  "cd ~/;ssr-server/ss-fly.sh -ssr"

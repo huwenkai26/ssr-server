4745af8400e38b5efb1a1bf5038cabeb9cd9c813
hwk <huwenkai@yuntutv.net>
2019-01-07 15:33:20 +0800

    frist commit

---
+++ run-ssr.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+check_os() {
+    if [[ -f /etc/redhat-release ]]; then
+        os="centos"
+    elif cat /etc/issue | grep -Eqi "debian"; then
+        os="debian"
+    elif cat /etc/issue | grep -Eqi "ubuntu"; then
+        os="ubuntu"
+    elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+        os="centos"
+    elif cat /proc/version | grep -Eqi "debian"; then
+        os="debian"
+    elif cat /proc/version | grep -Eqi "ubuntu"; then
+        os="ubuntu"
+    elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+        os="centos"
+    fi
+}
+check_os
+case $os in
+        'ubuntu'|'debian')
+     apt-get -y update
+             apt-get -y install git
+             ;;
+        'centos')
+             yum install -y git
+             ;;
+esac
+git clone
+ss-fly/ss-fly.sh -ssr
+

d64383fa39c95ae42de4c98adbe2c6f1ae6706ea
hwk <huwenkai@yuntutv.net>
2019-01-08 10:53:45 +0800

    添加切换ip接口

--- httpServer.py
+++ httpServer.py
@@ -1,14 +1,22 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
+import os
+import time
+import sys
 import threading
 # curl http://pv.sohu.com/cityjson?ie=utf-8
 class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        host = sys.argv[1]
+        port = sys.argv[2]
         self.send_response(200)
         self.end_headers()
 
-        message = "ssss"
+        os.popen('ssh root@'+host +' -p '+port+'\" adsl-stop;adsl start\"')
+        time.sleep(4)
+        out = os.popen('ssh root@'+host +' -p '+port+'\"curl  ipinfo.io/ip\"')
+        message = out.read();
         self.wfile.write(message)
         self.wfile.write('\n')
         return
@@ -17,6 +25,6 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in a separate thread."""
 
 if __name__ == '__main__':
-    server = ThreadedHTTPServer(('localhost', 8888), Handler)
+    server = ThreadedHTTPServer(('0.0.0.0', 8888), Handler)
     print 'Starting server, use <Ctrl-C> to stop'
     server.serve_forever()

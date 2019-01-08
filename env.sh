4a222418124c1722cf786a7e75b09619afad2e19
hwk <huwenkai@yuntutv.net>
2019-01-08 19:28:21 +0800

    检测网络

--- env.sh
+++ env.sh
@@ -16,6 +16,14 @@ check_os() {
         os="centos"
     fi
 }
+ping -c 1 114.114.114.114 > /dev/null 2>&1
+if [ $? -eq 0 ];then
+    echo 检测网络正常
+else
+    echo 检测网络连接异常
+    adsl-start
+fi
+
 check_os
 case $os in
         'ubuntu'|'debian')
